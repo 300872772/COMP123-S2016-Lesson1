1ce5fbcbe1ef8a1762b858042c2c0ff388b6aa25
300872772 <mamun_131@hotmail.com>
2016-05-10 23:41:33 -0400

    Update Visual Studio 2015

---
+++ hellowworldproject/HelloWorldUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace hellowworldproject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void HelloWorldUnitTestConsolTestMethod()
+        {
+            //Arrange
+            string expectedResult = "Hello World";
+            //Act
+            string result = COMP123_S2016_Lesson1.Program.HolowWorld();
+            //Asert   
+        }
+    }
+}
